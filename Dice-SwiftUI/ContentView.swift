dd9401078866700ac67cf14da30054d4a789a9a6
Pavel Chernook <Maklauren97@gmail.com>
2022-07-12 22:26:11 +0300

    Initial commit

---
+++ Dice-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dice-SwiftUI
+//
+//  Created by Павел Черноок on 12.07.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dice-SwiftUI/Dice_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_SwiftUIApp.swift
+//  Dice-SwiftUI
+//
+//  Created by Павел Черноок on 12.07.22.
+//
+
+import SwiftUI
+
+@main
+struct Dice_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
