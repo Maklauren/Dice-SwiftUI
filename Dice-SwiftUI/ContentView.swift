8a596109bc1c49bc5b7bafb382856b22cac20365
Pavel Chernook <Maklauren97@gmail.com>
2022-07-13 11:05:37 +0300

    Designing a layout using Spacers and Subviews

--- Dice-SwiftUI/ContentView.swift
+++ Dice-SwiftUI/ContentView.swift
@@ -9,7 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+                .aspectRatio(contentMode: .fill)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(diceNumber: 1)
+                    DiceView(diceNumber: 1)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button {
+                    
+                } label: {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(.red)
+                Divider()
+            }
+        }
+    }
+}
+
+struct DiceView: View {
+    
+    let diceNumber: Int
+    
+    var body: some View {
+        Image("dice\(diceNumber)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .frame(width: 125, height: 125)
             .padding()
     }
 }
